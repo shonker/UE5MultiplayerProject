6587b4383a7e68b78af2a1dd84e1e569cff50b22
Cayden Bullock <bullockcayden@gmail.com>
2023-04-20 11:11:29 -0700

    idle/standing animation implementation&housekeeping

--- Source/Blaster/Character/BlasterAnimInstance.cpp
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -48,7 +48,7 @@ void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     CharacterRotation = BlasterCharacter->GetActorRotation();
     const FRotator Delta = UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame);
     const float Target = Delta.Yaw / DeltaSeconds;
-    const float Interp = FMath::FInterpTo(Lean, Target, DeltaSeconds, 6.f);
+    const float Interp = FMath::FInterpTo(Lean, Target, DeltaSeconds, 1.f);
     Lean = FMath::Clamp(Interp, -90.f, 90.f);
 
 
